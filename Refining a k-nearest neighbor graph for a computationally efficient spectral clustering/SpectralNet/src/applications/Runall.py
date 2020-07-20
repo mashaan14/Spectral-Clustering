94353627163fe2cb67a991fe6eec7eb4b140f9d5
Mashaan <61033787+mashaan14@users.noreply.github.com>
2020-07-20 12:50:16 +1000

    Add files via upload

---
+++ Refining a k-nearest neighbor graph for a computationally efficient spectral clustering/SpectralNet/src/applications/Runall.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 12 22:52:41 2020
+
+@author: mals6571
+"""
+
+
+for r in range(10):
+    exec(open("C:/Users/mals6571/Desktop/SpectralNet-master/src/applications/run.py").read())
